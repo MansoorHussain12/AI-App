6ef319fb456e3a554fc1356310f17988dc9aa3ff
Mansoor Hussain <qaisrani07@gmail.com>
2026-02-07 12:03:25 +0500

    Add prisma client

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import { chatController } from './controllers/chat.controller';
+import { prisma } from './prisma/PrismaClient';
 
 const router = express.Router();
 
@@ -9,5 +10,7 @@ router.get('/', (req: Request, res: Response) => {
 });
 
 router.post('/api/chat', chatController.sendMessage);
+
+router.get('/api/products/:id/reviews', (req: Request, res: Response) => {});
 
 export default router;
